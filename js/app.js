910eb6909d154542f1bcf4eed72a5807dfbb57df
WahidaAkhter <wahidacse280@gmail.com>
2022-03-01 13:52:59 +0600

    search option functional work added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,44 @@
-const allPhone = () => {
-    const searchValue = document.getElementById("search-box").value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => DisplayPhoneDetails(data.phone));
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+//form event listener
+searchForm.addEventListener('submit', e => {
 
-};
-const DisplayPhoneDetails = (phones) => {
-    console.log(phones);
-};
+    //get search term 
+    const searchTerm = searchInput.value;
+    //get limit
+    const searchLimit = document.getElementById('limit').value;
+
+    //check input
+    if (searchTerm === '') {
+        //show message
+        showMessage('please put relevant name', 'alert-danger');
+    }
+
+    // Clear field
+    searchInput.value = '';
+
+
+    e.preventDefault();
+
+});
+// Show Message Function
+function showMessage(message, className) {
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const searchContainer = document.getElementById('search-container');
+    // Get form
+    const search = document.getElementById('search');
+
+    // Insert alert
+    searchContainer.insertBefore(div, search);
+
+    // Timeout after 3 sec
+    setTimeout(function () {
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
