416de1e7be22f49ef9a16edb7175001938b83961
WahidaAkhter <wahidacse280@gmail.com>
2022-03-02 13:22:48 +0600

    freshly done html and js part

--- js/app.js
+++ js/app.js
@@ -1,44 +1,37 @@
-const searchForm = document.getElementById('search-form');
-const searchInput = document.getElementById('search-input');
-//form event listener
-searchForm.addEventListener('submit', e => {
 
-    //get search term 
-    const searchTerm = searchInput.value;
-    //get limit
-    const searchLimit = document.getElementById('limit').value;
 
-    //check input
-    if (searchTerm === '') {
-        //show message
-        showMessage('please put relevant name', 'alert-danger');
+// search phone
+const allPhones = () => {
+    const searchField = document.getElementById("search-field").value
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchField}`
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => showPhoneDetails(data.data));
+};
+
+const showPhoneDetails = phones => {
+    for (const phone of phones) {
+        const parent = document.getElementById('phone-container');
+        const div = document.createElement('div');
+        div.innerHTML = `<div class="card border p-5">
+            <div class="phone-pic">
+                <img class="w-25" src="${phone.image}" alt="">
+            </div>
+            <h2>Name: '${phones.brand}' cool</h2>
+            <h5>Model: </h5>
+            <p></p>
+            <div class="all-button">
+                <button class="btn btn-danger">Delete</button>
+                <button class="btn btn-success">Details</button>
+            </div>
+       </div>`;
+        parent.appendChild(div);
     }
 
-    // Clear field
-    searchInput.value = '';
+
+};
 
 
-    e.preventDefault();
 
-});
-// Show Message Function
-function showMessage(message, className) {
-    // Create div
-    const div = document.createElement('div');
-    // Add classes
-    div.className = `alert ${className}`;
-    // Add text
-    div.appendChild(document.createTextNode(message));
-    // Get parent
-    const searchContainer = document.getElementById('search-container');
-    // Get form
-    const search = document.getElementById('search');
 
-    // Insert alert
-    searchContainer.insertBefore(div, search);
 
-    // Timeout after 3 sec
-    setTimeout(function () {
-        document.querySelector('.alert').remove();
-    }, 3000);
-}
